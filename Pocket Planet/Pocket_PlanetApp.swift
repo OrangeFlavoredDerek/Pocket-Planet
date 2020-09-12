ad5da3f9c3a17fa3599c2c0279ad587eb58dc5a7
Derek <chenjunqi07@icloud.com>
2020-09-12 15:00:32 +0800

    Initial Commit

---
+++ Pocket Planet/Pocket_PlanetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pocket_PlanetApp.swift
+//  Pocket Planet
+//
+//  Created by Derek Chan on 2020/9/12.
+//
+
+import SwiftUI
+
+@main
+struct Pocket_PlanetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
