302c0b4df053abee4a4aee289df5c1036513738e
Derek <chenjunqi07@icloud.com>
2020-09-13 16:21:20 +0800

    # update

--- Pocket Planet/HomeView.swift
+++ Pocket Planet/HomeView.swift
@@ -7,15 +7,16 @@
 
 import SwiftUI
 import SceneKit
+import UserNotificationsUI
 
 struct HomeView: View {
     @State var models = [
+        Model(id: 0, name: "Earth", modelName: "Earth.usdz", details: "Earth is the third planet from the Sun and the only astronomical object known to harbor life. According to radiometric dating estimation and other evidence, Earth formed over 4.5 billion years ago. Earth's gravity interacts with other objects in space, especially the Sun and the Moon, which is Earth's only natural satellite. Earth orbits around the Sun in 365.256 solar days, a period known as an Earth sidereal year. During this time, Earth rotates about its axis 366.256 times; that is, a sidereal year has 366.256 sidereal days."),
         Model(id: 0, name: "Sun", modelName: "Sun.usdz", details: "The Sun is the star at the center of the Solar System. It is a nearly perfect sphere of hot plasma, heated to incandescence by nuclear fusion reactions in its core, radiating the energy mainly as light and infrared radiation. It is by far the most important source of energy for life on Earth. Its diameter is about 1.39 million kilometers (864,000 miles), or 109 times that of Earth, and its mass is about 330,000 times that of Earth. It accounts for about 99.86% of the total mass of the Solar System. Roughly three quarters of the Sun's mass consists of hydrogen (~73%); the rest is mostly helium (~25%), with much smaller quantities of heavier elements, including oxygen, carbon, neon, and iron."),
         Model(id: 0, name: "Mercury", modelName: "Mercury.usdz", details: "Mercury is the smallest and innermost planet in the Solar System. Its orbit around the Sun takes 87.97 days, the shortest of all the planets in the Solar System. It is named after the Greek god Hermes (Ερμής), translated into Latin Mercurius Mercury, god of commerce, messenger of the gods, mediator between gods and mortals."),
         Model(id: 0, name: "Venus", modelName: "Venus.usdz", details: "Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty. As the second-brightest natural object in the night sky after the Moon, Venus can cast shadows and can be, on rare occasion, visible to the naked eye in broad daylight. Venus lies within Earth's orbit, and so never appears to venture far from the Sun, either setting in the west just after dusk or rising in the east a bit before dawn. Venus orbits the Sun every 224.7 Earth days. With a rotation period of 243 Earth days, it takes longer to rotate about its axis than any other planet in the Solar System and does so in the opposite direction to all but Uranus (meaning the Sun rises in the west and sets in the east). Venus does not have any moons, a distinction it shares only with Mercury among planets in the Solar System."),
-        Model(id: 0, name: "Earth", modelName: "Earth.usdz", details: "Earth is the third planet from the Sun and the only astronomical object known to harbor life. According to radiometric dating estimation and other evidence, Earth formed over 4.5 billion years ago. Earth's gravity interacts with other objects in space, especially the Sun and the Moon, which is Earth's only natural satellite. Earth orbits around the Sun in 365.256 solar days, a period known as an Earth sidereal year. During this time, Earth rotates about its axis 366.256 times; that is, a sidereal year has 366.256 sidereal days."),
-        Model(id: 0, name: "Moon", modelName: "Moon.usdz", details: "The Moon is an astronomical body orbiting Earth and is the planet's only natural satellite. It is the fifth-largest satellite in the Solar System, and by far[13] the largest among planetary satellites relative to the size of the planet that it orbits. The Moon is, after Jupiter's satellite Io, the second-densest satellite in the Solar System among those whose densities are known."),
-        Model(id: 0, name: "Mars", modelName: "Mars.usdz", details: "Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being only larger than Mercury. In English, Mars carries the name of the Roman god of war and is often referred to as the 'Red Planet'. The latter refers to the effect of the iron oxide prevalent on Mars's surface, which gives it a reddish appearance distinctive among the astronomical bodies visible to the naked eye.[19] Mars is a terrestrial planet with a thin atmosphere, with surface features reminiscent of the impact craters of the Moon and the valleys, deserts and polar ice caps of Earth."),
+        Model(id: 0, name: "Moon", modelName: "Moon.usdz", details: "The Moon is an astronomical body orbiting Earth and is the planet's only natural satellite. It is the fifth-largest satellite in the Solar System, and by far the largest among planetary satellites relative to the size of the planet that it orbits. The Moon is, after Jupiter's satellite Io, the second-densest satellite in the Solar System among those whose densities are known."),
+        Model(id: 0, name: "Mars", modelName: "Mars.usdz", details: "Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being only larger than Mercury. In English, Mars carries the name of the Roman god of war and is often referred to as the 'Red Planet'. The latter refers to the effect of the iron oxide prevalent on Mars's surface, which gives it a reddish appearance distinctive among the astronomical bodies visible to the naked eye. Mars is a terrestrial planet with a thin atmosphere, with surface features reminiscent of the impact craters of the Moon and the valleys, deserts and polar ice caps of Earth."),
         Model(id: 0, name: "Jupiter", modelName: "Jupiter.usdz", details: "Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass one-thousandth that of the Sun, but two-and-a-half times that of all the other planets in the Solar System combined. Jupiter is one of the brightest objects visible to the naked eye in the night sky, and has been known to ancient civilizations since before recorded history. It is named after the Roman god Jupiter. When viewed from Earth, Jupiter can be bright enough for its reflected light to cast visible shadows, and is on average the third-brightest natural object in the night sky after the Moon and Venus."),
         Model(id: 0, name: "Saturn", modelName: "Saturn.usdz", details: "Saturn is the sixth planet from the Sun and the second-largest in the Solar System, after Jupiter. It is a gas giant with an average radius of about nine times that of Earth. It only has one-eighth the average density of Earth; however, with its larger volume, Saturn is over 95 times more massive. Saturn is named after the Roman god of wealth and agriculture; its astronomical symbol (♄) represents the god's sickle."),
         Model(id: 0, name: "Uranus", modelName: "Uranus.usdz", details: "Uranus is the seventh planet from the Sun. Its name is a reference to the Greek god of the sky, Uranus, who, according to Greek mythology, was the grandfather of Zeus (Jupiter) and father of Cronus (Saturn). It has the third-largest planetary radius and fourth-largest planetary mass in the Solar System. Uranus is similar in composition to Neptune, and both have bulk chemical compositions which differ from that of the larger gas giants Jupiter and Saturn. For this reason, scientists often classify Uranus and Neptune as 'ice giants' to distinguish them from the other gas giants. Uranus' atmosphere is similar to Jupiter's and Saturn's in its primary composition of hydrogen and helium, but it contains more 'ices' such as water, ammonia, and methane, along with traces of other hydrocarbons. It has the coldest planetary atmosphere in the Solar System, with a minimum temperature of 49 K (−224 °C; −371 °F), and has a complex, layered cloud structure with water thought to make up the lowest clouds and methane the uppermost layer of clouds. The interior of Uranus is mainly composed of ices and rock."),
@@ -29,17 +30,69 @@ struct HomeView: View {
         VStack {
             SceneView(scene: SCNScene(named: models[index].modelName), options: [.autoenablesDefaultLighting, .allowsCameraControl])
                 .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 2)
+                .edgesIgnoringSafeArea(.top)
             
             ZStack{
+//                ScrollView(.horizontal, showsIndicators: false){
+//
+//                }
                 HStack{
-                    Button(action: {}){
+                    Button(action: {
+                        vibrationFeedback()
+                        withAnimation {
+                            if index > 0 {
+                                index -= 1
+                            }
+                        }
+                    }){
                         Image(systemName: "chevron.left")
+                            .foregroundColor(.primary)
+                            .font(.system(size: 35, weight: .bold, design: .rounded))
+                            .opacity(index == 0 ? 0.3 : 1)
                     }
+                    .disabled(index == 0 ? true : false)
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        vibrationFeedback()
+                        withAnimation {
+                            if index < models.count {
+                                index += 1
+                            }
+                        }
+                    }){
+                        Image(systemName: "chevron.right")
+                            .foregroundColor(.primary)
+                            .font(.system(size: 35, weight: .bold, design: .rounded))
+                            .opacity(index == models.count - 1 ? 0.3 : 1)
+                    }
+                    .disabled(index == models.count - 1 ? true : false)
                 }
+                .padding()
+                
+                Text(models[index].name)
+                    .foregroundColor(.primary)
+                    .font(.system(size: 45, weight: .bold, design: .monospaced))
             }
+            .foregroundColor(.black)
+            
+            ScrollView([.vertical], showsIndicators: false){
+//                Text("About")
+//                    .foregroundColor(.primary)
+//                    .font(.title2)
+//                    .bold()
+                Text(models[index].details)
+                    .foregroundColor(.primary)
+            }
+            .padding(.horizontal)
             
             Spacer()
         }
+    }
+    func vibrationFeedback() {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.warning)
     }
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ### Pocket Planet SwiftUI 2.0 Version
+
+#### Requirement
+* iOS 14.0 +
+* macOS Big Sur
+* Xcode 12
